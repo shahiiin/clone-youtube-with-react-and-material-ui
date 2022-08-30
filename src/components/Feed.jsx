3e8cb3f6ed86e6f4c8bffb7273769939517d2a23
shahin <shahin.shahi1986@gmail.com>
2022-08-30 14:32:18 +0300

    side bar is ready

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,14 +1,18 @@
 import { useState, useEffect } from 'react'
 import { Box, Typography, Stack } from '@mui/material'
+import Sidebar from './Sidebar'
 
 const Feed = () => {
   return (
     <Stack sx={{ flexDirection: { sx: "column", md: "row" } }}>
       <Box sx={{ height: { sx: "auto", md: "92vh" }, borderRight: "1px solid #3d3d3d", px: { sx: 0, md: 2 } }}>
-        Sidebar
+        <Sidebar />
         <Typography className="copyright" variant="body2" sx={{ mt: 1.5, color: "#fff", }}>
           Copyright © 2022 JSM Media
         </Typography>
+      </Box>
+      <Box>
+        
       </Box>
     </Stack>
   )
