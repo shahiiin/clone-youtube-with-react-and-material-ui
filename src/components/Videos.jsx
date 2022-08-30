712c4566794671bcc98ee13b2c9e16b03059478d
shahin <shahin.shahi1986@gmail.com>
2022-08-30 17:25:23 +0300

    call api is sucssesfully

--- src/components/Videos.jsx
+++ src/components/Videos.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 
-const Videos = () => {
+const Videos = ({videos}) => {
+console.log(videos,'jhj')
   return (
     <div>Videos</div>
   )
--- src/utils/fetchFromApi.js
+++ src/utils/fetchFromApi.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const BASE_URL = 'https://youtube-v31.p.rapidapi.com'
+export const BASE_URL = 'https://youtube-v31.p.rapidapi.com'
 
 const options = {
   url: BASE_URL,
@@ -8,11 +8,12 @@ const options = {
     maxResults: '50',
   },
   headers: {
-    'X-RapidAPI-Key': process.env.RAPID_API,
+    'X-RapidAPI-Key': '079e150c67mshe04008bbadb9cb8p1429bajsnb83adbde9277',
     'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com',
   },
 }
 export const fetchFromApi = async (url) => {
   const { data } = await axios.get(`${BASE_URL}/${url}`, options)
+
   return data
 }
