fdf0cbd5944135206b76dae3963111d39c9a3b9a
shahin <shahin.shahi1986@gmail.com>
2022-08-30 14:56:47 +0300

    make video component

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react'
 import { Box, Typography, Stack } from '@mui/material'
-import Sidebar from './Sidebar'
+import {Sidebar,Videos} from './'
+
 
 const Feed = () => {
   return (
@@ -11,8 +12,11 @@ const Feed = () => {
           Copyright © 2022 JSM Media
         </Typography>
       </Box>
-      <Box>
-        
+      <Box p={2} sx={{ overflowY: "auto", height: "90vh", flex: 2 }}>
+        <Typography variant="h4" fontWeight="bold" mb={2} sx={{ color: "white" }}>
+          New <span style={{ color: "#FC1503" }}>videos</span>
+        </Typography>
+        <Videos videos={[]}/>
       </Box>
     </Stack>
   )
