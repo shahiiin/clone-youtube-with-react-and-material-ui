c018b54c857a65cfc58674f24d29a0f082f42e57
shahin <shahin.shahi1986@gmail.com>
2022-08-30 14:12:19 +0300

    start make sidebar

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -5,29 +5,29 @@ import SearchIcon from '@mui/icons-material/Search';
 
 
 const SearchBar = () => {
-  return (
-    <Paper
-    component='form'
-    onSubmit=''
-    sx={{
-      borderRadius: 20,
-      border: '1px solid #e3e3e3',
-      pl: 2,
-      boxShadow: 'none',
-      mr: { sm: 5 },
-    }}
-  >
-    <input
-      className='search-bar'
-      placeholder='Search...'
-      value=''
-    //   onChange={(e) => setSearchTerm(e.target.value)}
-    />
-    <IconButton type='submit' sx={{ p: '10px', color: 'red' }} aria-label='search'>
-      <SearchIcon />
-    </IconButton>
-  </Paper>
-  )
+    return (
+        <Paper
+            component='form'
+            onSubmit=''
+            sx={{
+                borderRadius: 20,
+                border: '1px solid #e3e3e3',
+                pl: 2,
+                boxShadow: 'none',
+                mr: { sm: 5 },
+            }}
+        >
+            <input
+                className='search-bar'
+                placeholder='Search...'
+                value=''
+            //   onChange={(e) => setSearchTerm(e.target.value)}
+            />
+            <IconButton type='submit' sx={{ p: '10px', color: 'red' }} aria-label='search'>
+                <SearchIcon />
+            </IconButton>
+        </Paper>
+    )
 }
 
 export default SearchBar
