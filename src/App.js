bd937409a25ca516d96b899a6abff61f2f88b86b
shahin <shahin.shahi1986@gmail.com>
2022-08-30 10:54:49 +0300

    make route and new component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { Box } from '@mui/material'
+import Navbar from './components/Navbar'
 
 
 function App() {
   return (
-    <div className="App">
-   grsgdfdfclea
-    </div>
-  );
+    <BrowserRouter>
+      <Box sx={{ backgroundColor: '#000' }}>
+        <Navbar/>
+        <Routes>
+          <Route path="/" exact element={<Feed />} />
+          <Route path="/video/:id"  element={<VideoDetile />} />
+          <Route path="/channel/:id"  element={<ChannelDetile />} />
+          <Route path="/search/:searchTerm"  element={<SearchFeed />} />
+        </Routes>
+      </Box>
+    </BrowserRouter>
+  )
 }
 
-export default App;
+export default App
